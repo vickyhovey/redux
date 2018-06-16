de6f475591fc68ff4a3544d610fe451182cf0480
vickyhovey <vickyhovey@gmail.com>
2018-06-16 14:58:20 -0400

    Array destructuring

--- redux/src/playground/destructuring.js
+++ redux/src/playground/destructuring.js
@@ -1,11 +1,16 @@
+//
+// Object destructuring
+//
+
 // const person = {
 //   name: 'Jing',
 //   age: 27,
 //   location: {
 //     city: 'New York',
-//     temp: 88
+//     temp: 92
 //   }
 // };
+
 // const { name: firstName = 'Anonymous', age } = person;
 // console.log(`${firstName} is ${age}.`);
 
@@ -14,18 +19,27 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-// const { name, age } = person;
-// console.log(`${name} is ${age}.`);
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name: 'Penguin'
+//   }
+// };
 
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    // name: 'Penguin'
-  }
-};
+// console.log(publisherName); // Penguin, Self-Published
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
+//
+// Array destructuring
+//
 
-console.log(publisherName); // Penguin, Self-Published
+// const address = ['257 Gold Street', 'Brooklyn', 'NY', '11201'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
